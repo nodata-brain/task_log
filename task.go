99652f96e4ee1a568c49c8895f474ea3718796ce
nodata-brain <nodata.eng@gmail.com>
2019-05-22 19:12:17 +0900

    add write task

--- task.go
+++ task.go
@@ -12,10 +12,18 @@ func main() {
 	task := flag.String("l", "Other", "task_type")
 	flag.Parse()
 
-	//file書き込み
-	err := ioutil.WriteFile("task.txt", []byte(*task), 0755)
-
+	//file読み込み
+	read_task, err := ioutil.ReadFile("task.txt")
 	if err != nil {
+		panic(err)
+	}
+
+	new_task := append(read_task, *task...)
+
+	//file書き込み
+	file_err := ioutil.WriteFile("task.txt", new_task, 0755)
+
+	if file_err != nil {
 		panic(err)
 	}
 
